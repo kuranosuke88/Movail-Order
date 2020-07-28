cde9244a1b3ff24e0550c322a9228c9ec188d24b
kuranosuke88 <takorou88@yahoo.co.jp>
2020-07-28 22:05:55 +0900

    topページ作成＆slim導入完了

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MovailOrder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = 'Asia/Tokyo' #日時取り扱い
+    config.generators.template_engine = :slim #slimに変換
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
